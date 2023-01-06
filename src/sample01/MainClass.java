5affd6d2e3beac8d41de44cbc96574e06e5a94e8
onlyoy <onlyoy4@naver.com>
2023-01-06 14:31:42 +0900

    workproject

---
+++ src/sample01/MainClass.java
@@ -0,0 +1,40 @@
+package sample01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("헬로우 자바");
+		
+		// 한줄 주석문
+		
+		System.out.println("Hello Wrold");
+		
+		System.out.print("Hello");
+		
+		System.out.print("월드");
+		
+		// 기능 : escape sequence
+		//      \n(개행), \b(backspace), \"(따옴표를 출력), \t(띄어쓰기)
+		
+		System.out.print("World\n");
+		
+		System.out.println("헬로우\b");
+		
+		System.out.println("\"Hello Wrold\"");
+		
+		System.out.println("\tHello\tWrold");
+		
+		System.out.println("\t월드\t헬로");
+		
+		System.out.println("\\헬로우 월드\\");
+		
+		System.out.printf("%s \n", "Hello");
+		
+		System.out.println(1024);
+		
+		System.out.println("Hello" + "World");
+	}
+
+}
